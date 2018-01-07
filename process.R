8795380b93063dfdd21c5f298466043b8d546969
David Shaub <davidshaub@gmx.com>
2018-01-06 17:30:26 -0700

    more functions

--- process.R
+++ process.R
@@ -1,16 +1,26 @@
 library(data.table)
 library(forecastHybrid)
 
-determineType(df){
-    return substr(head(V1$, 1), 0, 1)
+# Load all the data files
+files <- dir(pattern = "*.csv")
+
+determineType <- function(df){
+    # For each data file, determine if it is daily, hourly, weekly, etc
+    return(substr(head(df$V1, 1), start = 0, 1))
     }
 
+getHorizon <- function(x){
+    # Determine the forecast horizon
+    horizon <- c("H" = 48, "D" = 14, "W" = 13, "M" = 18, "Y" = 6)
+    return(horizon[x])
+    }
 
 processFile <- function(x){
+    # Load the data, train a model, produce forecasts, and write results
     dat <- fread(x, header = TRUE)
     type <- determineType()
     dat$V1 <- NULL
     horizon <- getHorizon()
-    dat = dat[, lapply(.SD, as.numeric), by=ID]
-    return dat
+    dat = dat[, lapply(.SD, as.numeric)]
+    return(dat)
     }
