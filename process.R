2a6643f25b3741950415da73a9c1a0e227d3d0dc
David Shaub <davidshaub@gmx.com>
2018-01-07 09:00:35 -0700

    fewer features

--- process.R
+++ process.R
@@ -11,13 +11,13 @@ files <- dir(pattern = "*.csv")
 
 cleanM <- function(mObj){
     for(i in seq_along(mObj)){
-        if(grepl("DEMOGR", mObj[[i]]$type)){
+        if(grepl("DEMOGR", mObj[[i]]$type, ignore.case = TRUE)){
             mObj[[i]]$type <- "DEMOGRAPHIC"
-        } else if(grepl("INDUST", mObj[[i]]$type)){
+        } else if(grepl("INDUST", mObj[[i]]$type, ignore.case = TRUE)){
             mObj[[i]]$type <- "INDUSTRY"
-        } else if(grepl("MACRO", mObj[[i]]$type)){
+        } else if(grepl("MACRO", mObj[[i]]$type, ignore.case = TRUE)){
             mObj[[i]]$type <- "MACRO"
-        } else if(grepl("MICRO", mObj[[i]]$type)){
+        } else if(grepl("MICRO", mObj[[i]]$type, ignore.case = TRUE)){
             mObj[[i]]$type <- "MICRO"
             }
         }
@@ -28,19 +28,24 @@ data(M3)
 data(M1)
 allData <- c(M1, M3)
 
+# All possible models
+models <- c("a", "e", "f", "n", "s", "t")
+expandedGrid <- expand.grid(rep(list(models), times = length(models)))
+noDupes <- unique(apply(expandedGrid, 1, FUN = function(x) sort(unique(x))))
+allModels <- noDupes[sapply(noDupes, FUN = function(x) length(x) >= 2)]
+allModels <- sapply(allModels, FUN = function(x) paste0(x, collapse=""))
+
 tsFeatures <- function(x){
     options(warn=-1)
     adf <- adf.test(x)
     scaled <- scale(x)
     min_x = min(scaled)
     max_x = max(scaled)
-    s_trend = as.numeric(abs(coef(tslm(x ~ trend + season, x))["trend"]))
-    tsFreq <- frequency(x)
-    ch <- 0
-    # Strange assignment behavior from ch.test does not work in ifelse()
-    if(tsFreq){
-        ch <- ch.test(x)
-        }
+    tsFreq <- frequency(x) > 1
+    s_trend = ifelse(tsFreq,
+                     as.numeric(abs(coef(tslm(x ~ trend + season, x))["trend"])),
+                     as.numeric(abs(coef(tslm(x ~ trend, x))["trend"])))
+    trend2 <- AIC(tslm(x ~ trend, x)) > AIC(tslm(x ~ trend + I(trend^2), x))
     pp <- pp.test(x)
     pp_explosive <- pp.test(x, alternative = "explosive")
     arima_order <- tryCatch({auto.arima(x, method = "CSS")$arma},
@@ -52,16 +57,15 @@ tsFeatures <- function(x){
                     adf_statistic = as.numeric(adf$statistic),
                     adf_p = as.numeric(adf$p.value),
                     s_trend = s_trend,
-                    var = var(scaled),
+                    trend2 = trend2,
+                    var = var(x),
                     min = min_x,
                     max = max_x,
                     abs = max(abs(c(min_x, max_x))),
                     range = max(x) - min(x),
-                    skew = skewness(x),
-                    kurtosis = kurtosis(x),
+                    skew = abs(skewness(x)),
+                    kurtosis = abs(kurtosis(x)),
                     bc_lambda = BoxCox.lambda(x),
-                    ch_max = ifelse(tsFreq, max(ch$pvalues), 0),
-                    ch_mean = ifelse(tsFreq, mean(ch$pvalues), 0),
                     pp = pp$p.value,
                     pp_explosive = pp_explosive$p.value,
                     ar = arima_order[1],
@@ -74,6 +78,8 @@ tsFeatures <- function(x){
     options(warn=0)
     return(df)
     }
+
+allData <- cleanM(allData)
 
 determineType <- function(df){
     # For each data file, determine if it is daily, hourly, weekly, etc
