aadd97793566c887d585706bd777d79991f38ad2
David Shaub <davidshaub@gmx.com>
2018-01-19 09:28:41 -0700

    building datasets

--- modelSelector.R
+++ modelSelector.R
@@ -47,22 +47,23 @@ data(M3)
 data(M1)
 allData <- c(M1, M3, tourism)
 if(require(tscompdata)){
-	tscompdataPrepare(){
-		timeseries <- list(nn3, nn5, nngc1, gefcom2012_load, gefcom2012_temp, gefcom2012_wp)
-		}
-	}
+    tscompdataPrepare(){
+        timeseries <- list(nn3, nn5, nngc1, gefcom2012_load, gefcom2012_temp, gefcom2012_wp)
+        }
+    nn3Clean <- lapply(nn3, FUN = function(x) createMObject(x, type = "MICRO"))
+    }
 
 createMObject <- function(x, type){
-	xClean <- na.interp(x)
-	tsLength <- length(x)
-	horizon <- getHorizonFromFrequency(xClean)
-	testSet <- subset(xClean, start = tsLength - horizon + 1)
-	trainSet <- subset(xClean, end = tsLength - horizon)
-	returnList <- list(x = trainSet, x = testSet, h = horizon, n = length(trainSet), type = type)
-	return(returnList)
-	}
+    xClean <- na.interp(x)
+    tsLength <- length(x)
+    horizon <- getHorizonFromFrequency(xClean)
+    testSet <- subset(xClean, start = tsLength - horizon + 1)
+    trainSet <- subset(xClean, end = tsLength - horizon)
+    returnList <- list(x = trainSet, x = testSet, h = horizon, n = length(trainSet), type = type)
+    return(returnList)
+    }
 
-res <- lapply(nn3, FUN = function(x) createMObject(x, type = "MICRO"))
+
 cleaned <- cleanM(allData)
 # Shuffle data so it distributes evenly for parallel feature extraction
 set.seed(31415926)
--- process.R
+++ process.R
@@ -1,6 +1,3 @@
-
-
-
 determineType <- function(df){
     # For each data file, determine if it is daily, hourly, weekly, etc
     return(substr(head(df$V1, 1), start = 0, 1))
@@ -12,9 +9,9 @@ getHorizon <- function(x){
     return(horizon[x])
     }
 getHorizonFromFrequency <- function(x){
-	freq <- as.character(frequency(x))
-	switch(freq, "24" = 48, "365" = 14, "52" = 13, "12" = 18, "1" = 6)
-	}
+    freq <- as.character(frequency(x))
+    switch(freq, "24" = 48, "365" = 14, "52" = 13, "12" = 18, "1" = 6)
+    }
 
 prepareM <- function(data, period, type, names){
     seriesList <- list()
@@ -41,7 +38,7 @@ prepareDatasets <- function(){
     seriesNames <- names(nn5)
     NN5 <- prepareM(data = nn5, period = "DAILY", type = "OTHER", names = seriesNames)
     # nngc1 placeholder
-    
+}
 
 processFile <- function(x){
     # Load the data, train a model, produce forecasts, and write results
@@ -52,6 +49,3 @@ processFile <- function(x){
     dat = dat[, lapply(.SD, as.numeric)]
     return(dat)
     }
-
-
-################################################################################
