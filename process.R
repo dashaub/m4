5f4ed9824756bff25b835cd99604faf93659f443
David Shaub <davidshaub@gmx.com>
2018-01-21 13:09:20 -0700

    remove function

--- process.R
+++ process.R
@@ -8,10 +8,6 @@ getHorizon <- function(x){
     horizon <- c("HOURLY" = 48, "DAILY" = 14, "WEEKLY" = 13, "MONTHLY" = 18, "YEARLY" = 6)
     return(horizon[x])
     }
-getHorizonFromFrequency <- function(x){
-    freq <- as.character(frequency(x))
-    switch(freq, "24" = 48, "7" = 14, "52" = 13, "12" = 18, "1" = 6)
-    }
 
 prepareM <- function(data, period, type, names){
     seriesList <- list()
