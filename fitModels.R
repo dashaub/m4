b001e448cc902f1a8f36b7d179eb822c35f27f0d
David Shaub <davidshaub@gmx.com>
2018-01-23 12:13:06 -0700

    more cleanup

--- fitModels.R
+++ fitModels.R
@@ -11,7 +11,7 @@ fitModels <- function(x, models, lambda = FALSE){
     }
 fitModels <- cmpfun(fitModels, options = list(optimize = 3))
 
-getAccuracy <- function(mod, series){
+getAccuracy <- function(forecast, series){
     return(as.numeric(accuracy(mod, x = series$xx)["Test set", "MASE"]))
     }
 getAccuracy <- cmpfun(getAccuracy, options = list(optimize = 3))
@@ -26,37 +26,31 @@ fitModel <- function(series, method, lambda = FALSE){
     }
     # Too short for slm
     if(grepl("s", method) && frequency(series$xx) == 1){
-        NA
+        return(NA)
     } else if(length(series$x) / frequency(series$x) <= 2){
         # Too short
-        NA
+        return(NA)
     } else if(method == "a"){
         mod <- auto.arima(series$x, lambda = lambda)
         fc <- forecast(mod, h = length(series$xx))
-        as.numeric(accuracy(fc, x = series$xx)["Test set", "MASE"])
     } else if(method == "e"){
         mod <- ets(series$x, lambda = lambda)
         fc <- forecast(mod, h = length(series$xx))
-        as.numeric(accuracy(fc, x = series$xx)["Test set", "MASE"])
     } else if(method == "f"){
         mod <- thetam(series$x)
         fc <- forecast(mod, h = length(series$xx))
-        as.numeric(accuracy(fc, x = series$xx)["Test set", "MASE"])
     } else if(method == "n"){
         mod <- nnetar(series$x, lambda = lambda, MaxNWts = 10000)
         fc <- forecast(mod, h = length(series$xx))
-        as.numeric(accuracy(fc, x = series$xx)["Test set", "MASE"])
     } else if(method == "s"){
         mod <- stlm(series$x, lambda = lambda)
         fc <- forecast(mod, h = length(series$xx))
-        as.numeric(accuracy(fc, x = series$xx)["Test set", "MASE"])
     }else if(method == "t"){
         mod <- tbats(series$x, lambda = lambda)
         fc <- forecast(mod, h = length(series$xx))
-        as.numeric(accuracy(fc, x = series$xx)["Test set", "MASE"])
     } else if(method == "z"){
         fc <- snaive(series$x, h = length(series$xx), lambda = lambda)
-        as.numeric(accuracy(fc, x = series$xx)["Test set", "MASE"])
+        #as.numeric(accuracy(fc, x = series$xx)["Test set", "MASE"])
     }else{
         if(grepl("n", method)){
             maxwts <- list(MaxNWts = 10000)
@@ -66,8 +60,8 @@ fitModel <- function(series, method, lambda = FALSE){
         mod <- hybridModel(series$x, model = method, lambda = lambda,
                            verbose = FALSE, n.args = maxwts)
         fc <- forecast(mod, h = length(series$xx), PI = FALSE)
-        as.numeric(accuracy(fc, x = series$xx)["Test set", "MASE"])
         }
+    return(getAccuracy(fc, series))
     }
 fitModel <- cmpfun(fitModel, options = list(optimize = 3))
 
