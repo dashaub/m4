ecc276e64d8676788b94bca26e0ec0cf4e25ef05
David Shaub <davidshaub@gmx.com>
2018-05-31 10:22:39 -0600

    remove old files

--- daily.R
+++
@@ -1,9 +0,0 @@
-dailyForecast <- function(series){
-    # Build features
-    features <- tsFeatures(series$x)
-    # Select model
-    # Fit thief
-    # Forecast
-    # PI from theta
-    # return FC
-    }
