d4c519f7a079a7a9251e22a6c7977f2198ca2b3c
David Shaub <davidshaub@gmx.com>
2018-10-03 15:59:41 -0600

    save all models; analyze results

--- ijf/analysis.R
+++ ijf/analysis.R
@@ -2,14 +2,70 @@
 library(M4comp2018)
 library(pbapply)
 library(forecastHybrid)
+library(xtable)
+library(ggplot2)
 
 numCores <- 2
+
+####################################################################################################
+# Forecasts
+####################################################################################################
+
 
 # Full ensemble forecasts
 load("mods.RData")
 ensembleForecasts <- pblapply(mods, function(x) forecast(x), cl = numCores)
-rm(mods)
+save(ensembleForecßasts, file = "ensembleForecasts.RData")
+
+# Forecasts from the best model according to the model selection procedure
+load("bestMods.RData")
+selectionForecasts <- pblapply(bestMods, function(x) forecast(x), cl = numCores)
+save(selectionForecasts, file = "selectionForecasts.RData")
+
+# Forecasts from the best model given by on Oracle on the test set
+load("oracleMods.RData")
+oracleForecasts <- pblapply(oracleMods, function(x) forecast(x), cl = numCores)
+save(oracleForecasts, file = "oracleForecasts.RData")
+
+
+####################################################################################################
+# Tables
+####################################################################################################
+
+
+# LaTeX table of distribution of selected best individual models
+load("bestMods.RData")
+bestModDistribution <- table(sapply(bestMods, function(x) class(x)[1]))
+
+# LaTeX table of distribution of selected best individual models from oracle
+load("oracleMods.RData")
+oracleModDistribution <- table(sapply(oracleMods, function(x) class(x)[1]))
+
+
+####################################################################################################
+# MASE calculation
+####################################################################################################
+
+
+rm(oracleMods, bestMods, mods)
 gc()
 
-save(ensembleForecasts, file = "ensembleForecasts.RData")
+data(M4)
+# Calculuate MASE for a specific model
+calculateMASE <- function(fc){
+  dat <- M4[sapply(M4, function(i) i$period == "Yearly")]
+  lambda <- function(i) accuracy(fc[[i]], as.numeric(dat[[i]]$xx))["Test set", "MASE"]
+  res <- pblapply(seq_along(fc), lambda, cl = numCores)
+  res <- unlist(res)
+  return(res)
+}
+
+ensembleMASE <- calculateMASE(ensembleForecasts)
+selectionMASE <- calculateMASE(selectionForecasts)
+oracleMASE <- calculateMASE(oracleForecasts)
+save(ensembleMASE, oracleMASE, selectionMASE, file="MASE.RData")
+
+####################################################################################################
+# Distributions
+####################################################################################################
 
--- ijf/saveModels.R
+++ ijf/saveModels.R
@@ -1,9 +1,15 @@
 library(forecastHybrid)
 library(data.table)
 library(pbapply)
+library(M4comp2018)
 
 currentSeries <- "Yearly"
-numCores <- 4
+numCores <- 1
+
+
+####################################################################################################
+# Prepare data
+####################################################################################################
 
 
 # Extract the dataframe to a list of msts objects
@@ -37,11 +43,89 @@ dat <- apply(dat, MARGIN = 1, FUN = function(x) extractList(x, currentSeries))
 names(dat) <- seriesNames
 
 
-# Fit the ensemble model. This include both the model used in M4 forecasting as well as the
+####################################################################################################
+# Ensemble models
+####################################################################################################
+
+
+# Fit the ensemble model. This includes both the model used in M4 forecasting as well as the
 # individual component models that will be compared. These are the final fit models
 models <- ifelse(currentSeries == "Monthly", "fs", "aft")
 mods <- pblapply(dat, function(x) hybridModel(x, models = models, verbose = FALSE), cl = numCores)
 save(mods, file = "mods.RData")
+gc()
+
+
+####################################################################################################
+# Helper functions for model selection
+####################################################################################################
+
+
+# Combine the M4 train and test data for yearly
+combineTrainTestSet <- function(x){
+  dat <- x[sapply(x, function(i) i$period == "Yearly")]
+  res <- lapply(dat, function(i) c(i$x, i$xx))
+  return(res)
+}
+
+# Combine the model and data into a single list so we can easily parallelize
+packageModelsAndData <- function(models, data){
+  output <- lapply(seq_along(models), function(x) list(model = models[[x]], data = data[[x]]))
+  return(output)
+}
+
+# Chose a single "best" model based on the holdout set
+chooseBestModel <- function(x){
+  mod <- x$model
+  data <- x$data
+  # Create a holdout set
+  holdout <- as.numeric(tail(data, h))
+
+  bestModel <- NULL
+  bestMASE <- Inf
+  perfectModels <- list()
+  for(model in c("auto.arima", "tbats", "thetam")){
+    currentModel <- mod[[model]]
+    prediction <- forecast(currentModel, h=h)
+    currentMASE <- accuracy(prediction, holdout)["Test set", "MASE"]
+
+    if(all(prediction$mean == holdout)){
+      perfectModels[[model]] <- currentModel
+      next
+    }
+    if(currentMASE < bestMASE){
+      bestMASE <- currentMASE
+      bestModel <- currentModel
+    }
+  }
+
+  # If at least one model was a perfect fit, randomly select from those that fit perfectly
+  if(length(perfectModels)){
+    return(perfectModels[[sample(seq_along(perfectModels), 1)]])
+    }
+  return(bestModel)
+}
+
+
+####################################################################################################
+# Oracle model
+####################################################################################################
+
+data(M4)
+m4Data <- combineTrainTestSet(M4)
+modelsAndData <- packageModelsAndData(mods, m4Data)
+rm(mods, m4Data)
+gc()
+
+oracleMods <- pblapply(modelsAndData, function(x) chooseBestModel(x), cl = numCores)
+save(oracleMods, file = "oracleMods.RData")
+rm(oracleMods)
+gc()
+
+
+####################################################################################################
+# Subset train data models
+####################################################################################################
 
 
 # Fit again on the subset of the data. This is the data that will be used along with the holdout
@@ -51,34 +135,10 @@ subsetMods <- pblapply(dat, function(x) hybridModel(tail(x, -h), models = models
 save(subsetMods, file = "subsetMods.RData")
 
 
-# Chose a single "best" model based on the holdout set
-chooseBestModel <- function(x){
-  mod <- x$mod
-  data <- x$data
-  # Create a holdout set
-  holdout <- as.numeric(tail(data, h))
+####################################################################################################
+# Model selection for best component model on holdout set
+####################################################################################################
 
-  bestModel <- NULL
-  bestMASE <- Inf
-  for(model in c("auto.arima", "thetam", "tbats")){
-    prediction <- forecast(mod[[model]], h=h)
-    currentMASE <- accuracy(prediction, holdout)["Test set", "MASE"]
-    if(currentMASE < bestMASE){
-      bestMase <- currentMASE
-      bestModel <- mod[[model]]
-    }
-  }
-  return(bestModel)
-}
-
-# Combine the model and data into a single list so we can easily parallelize
-packageModelsAndData <- function(models, data){
-  output <- vector(mode = "list", length = length(models))
-  for(i in seq_along(models)){
-    output[[i]] <- list(model = models[[i]], data = data[[i]])
-  }
-  return(output)
-}
 
 modelsAndData <- packageModelsAndData(subsetMods, dat)
 rm(subsetMods)
